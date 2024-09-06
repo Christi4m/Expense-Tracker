72c9d9d8ee6d2719a49b7523d4ddc9c2012dfce8
Christi4m <christiam23_@hotmail.com>
2024-09-05 19:01:36 -0500

    ✨feature: complete branch expense and add front app base with vite

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { db } = require("./db/db");
+const { db } = require("./src/db/db");
 const { readdirSync } = require("fs");
 require("dotenv").config();
 const app = express();
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-readdirSync("./routes").map((route) =>
-  app.use("/api/v1", require("./routes/" + route))
+readdirSync("./src/routes").map((route) =>
+  app.use("/api/v1", require("./src/routes/" + route))
 );
 
 const server = () => {
---
+++ Backend/src/routes/transactions.js
@@ -0,0 +1,18 @@
+const { addIncome, getIcomes, deleteIncome } = require("../controllers/income");
+const {
+  addExpense,
+  getExpenses,
+  deleteExpense,
+} = require("../controllers/expense");
+
+const router = require("express").Router();
+
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIcomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpenses)
+  .delete("/delete-expense/:id", deleteExpense);
+
+module.exports = router;
